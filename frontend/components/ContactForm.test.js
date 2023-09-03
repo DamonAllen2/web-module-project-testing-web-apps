599cd0ef8b63180992780beb87df9f45c20017e0
Damon Allen <damon.nicholas.allen@live.com>
2023-09-03 12:58:56 -0400

    6 tests working as intended

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -20,19 +20,34 @@ test('renders ONE error message if user enters less then 5 characters into first
     const contactForm = render(<ContactForm />);
     const firstName = contactForm.container.querySelector('#firstName');
     userEvent.type(firstName, 'Eddy');
-    await screen.getByText(/error: firstname must have at least 5 characters./i)
+    await expect(screen.getByText(/firstname must have at least 5 characters./i)).toBeInTheDocument();
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-
+    render(<ContactForm />);
+    const submit = screen.getByText(/submit/i);
+    userEvent.click(submit);
+    await expect(screen.getByText(/firstname must have at least 5 characters./i)).toBeInTheDocument();
+    await expect(screen.getByText(/lastname is a required field./i)).toBeInTheDocument();
+    await expect(screen.getByText(/email must be a valid email address./i)).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-
+    const contactForm = render(<ContactForm />);
+    const firstName = contactForm.container.querySelector('#firstName');
+    const lastName = contactForm.container.querySelector('#lastName');
+    userEvent.type(firstName, 'Sherran');
+    userEvent.type(lastName, 'Ed');
+    const submit = screen.getByText(/submit/i);
+    userEvent.click(submit);
+    await expect(screen.getByText(/email must be a valid email address./i)).toBeInTheDocument();
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-
+    render(<ContactForm />);
+    const email = screen.getByPlaceholderText('bluebill1049@hotmail.com');
+    userEvent.type(email, 'testing');
+    await expect(screen.getByText(/email must be a valid email address./i)).toBeInTheDocument();
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
